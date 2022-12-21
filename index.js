7bad16e80dc16f9780ae3c2273a347685af24ced
foesublime <rylandcs@gmail.com>
2022-12-21 14:09:09 -0700

    added js types to index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// JavaScript Data Types 
+
+/* Primitive / Value Types */
+
+// string
+// number
+// boolean
+// undefined
+// null
+
+/* Reference Type / Structural Types */
+
+// Objects 
+// Arrays 
+// Functions
